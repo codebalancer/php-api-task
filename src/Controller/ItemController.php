066479a2c63de07afed93bedb86422d0b5576b4c
Dominic Frohlof <dominic.frohlof@gmail.com>
2021-07-28 18:06:53 +0200

    implement update endpoint which still fails

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -52,13 +52,21 @@ class ItemController extends AbstractController
     public function delete(Request $request, int $id) : JsonResponse
     {
         if (empty($id)) {
-            return $this->json(['error' => 'No data parameter'], Response::HTTP_BAD_REQUEST);
+            return $this->json(['error' => 'id parameter missing'], Response::HTTP_BAD_REQUEST);
         }
 
+        /**
+         * @var Item $item
+         */
         $item = $this->getDoctrine()->getRepository(Item::class)->find($id);
 
         if ($item === null) {
             return $this->json(['error' => 'No item'], Response::HTTP_BAD_REQUEST);
+        }
+
+        // cant delete someone else's data!
+        if ($item->getUser()->getId() != $this->getUser()->getId()){
+            return $this->json(['error' => ''], Response::HTTP_FORBIDDEN);
         }
 
         $manager = $this->getDoctrine()->getManager();
@@ -67,4 +75,46 @@ class ItemController extends AbstractController
 
         return $this->json([]);
     }
+
+    /**
+     * update existing item with new data for user
+     * @Route("/item/{id}", name="item_update", methods={"PUT"})
+     * @IsGranted("ROLE_USER")
+     */
+    public function update(Request $request, int $id, ItemService $itemService) : JsonResponse
+    {
+        if (empty($id)) {
+            return $this->json(['error' => 'id parameter missing'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $data = $request->get('data');
+        if (empty($data)) {
+            return $this->json(['error' => 'No data parameter']);
+        }
+
+        $item = $this->getDoctrine()->getRepository(Item::class)->find($id);
+        if (!$item instanceof Item) {
+            return $this->json(['error' => 'item with id does not exist'], Response::HTTP_NOT_FOUND);
+        }
+
+        $user = $this->getUser();
+        if (empty($user)) {
+            //should not happen due to authentication but well ...
+            return $this->json(['error' => 'user not found'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        //make sure it's the user's item
+        if ($item->getUser()->getId() != $user->getId()) {
+            return $this->json(['error' => 'user not found'], Response::HTTP_FORBIDDEN);
+        }
+
+        //update the item data
+        $item->setData($data);
+
+        $manager = $this->getDoctrine()->getManager();
+        $manager->flush();
+
+        return $this->json([]);
+    }
+
 }
